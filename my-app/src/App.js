6969f2bb955fcd43d43808466fdbc44ece39b440
Katie McBride <mcbride.katieb@gmail.com>
2020-02-08 12:31:19 -0600

    setup for components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,33 +2,37 @@ import React, { Component } from "react";
 import EmployeeCard from "./components/employeeCard";
 import Wrapper from "./components/Wrapper/Wrapper";
 import employees from "./employee.json";
+import EmployeeContainer from "./components/EmployeeContainer"
 
 
-class App extends Component {
+function App() {
 
-  /* state */
-  state = {
-    employees
-  };
+  return <EmployeeContainer />;
+
+  // /* state */
+  // state = {
+  //   employees
+  // };
 
 
-  /* render */
-  render() {
-    return (
-      <Wrapper>
-        <h1 className="title">Employee List</h1>
-        {this.state.employees.map(employee => (
-            <EmployeeCard
-            id={employee.id}
-            key={employee.id}
-            name={employee.name}
-            image={employee.image}
-            department={employee.department}
-            position={employee.position}
-          />
-        ))}
-      </Wrapper>
-    );
-  }
+  // /* render */
+  // render() {
+  //   return (
+  //     <Wrapper>
+  //       <h1 className="title">Employee List</h1>
+  //       {this.state.employees.map(employee => (
+  //           <EmployeeCard
+  //           id={employee.id}
+  //           key={employee.id}
+  //           name={employee.name}
+  //           image={employee.image}
+  //           department={employee.department}
+  //           position={employee.position}
+  //         />
+  //       ))}
+  //     </Wrapper>
+  //   );
+  // }
 }
+
 export default App;
